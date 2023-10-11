b73325c7ff27cd32d78a9df7dc76fcfda57dc6ec
riamems <membagloria@gmail.com>
2023-10-11 07:36:06 -0700

    simple shell work

--- path.c
+++ path.c
@@ -9,17 +9,17 @@
  */
 int is_executable(info_t *info, char *file_path)
 {
-    struct stat st;
+struct stat st;
 
-    (void)info;
-    if (!file_path || stat(file_path, &st))
-        return (0);
+(void)info;
+if (!file_path || stat(file_path, &st))
+return (0);
 
-    if (st.st_mode & S_IFREG)
-    {
-        return (1);
-    }
-    return (0);
+if (st.st_mode & S_IFREG)
+{
+return (1);
+}
+return (0);
 }
 
 /**
@@ -30,20 +30,21 @@ int is_executable(info_t *info, char *file_path)
  *
  * Return: pointer to the duplicated substring
  */
-char *extract_substring(char *source_str, int start_idx, int end_idx)
+char *extract_substring(char *source_str,
+int start_idx, int end_idx)
 {
-    static char buffer[1024];
-    int i = 0, k = 0;
+static char buffer[1024];
+int i = 0, k = 0;
 
-    for (k = 0, i = start_idx; i < end_idx; i++)
-    {
-        if (source_str[i] != ':')
-        {
-            buffer[k++] = source_str[i];
-        }
-    }
-    buffer[k] = '\0';
-    return (buffer);
+for (k = 0, i = start_idx; i < end_idx; i++)
+{
+if (source_str[i] != ':')
+{
+buffer[k++] = source_str[i];
+}
+}
+buffer[k] = '\0';
+return (buffer);
 }
 
 /**
@@ -54,45 +55,46 @@ char *extract_substring(char *source_str, int start_idx, int end_idx)
  *
  * Return: full path to the command if found, or NULL if not found
  */
-char *find_executable_in_path(info_t *info, char *path_var, char *command)
+char *find_executable_in_path(info_t *info,
+char *path_var, char *command)
 {
-    int i = 0, current_pos = 0;
-    char *full_path;
+int i = 0, current_pos = 0;
+char *full_path;
 
-    if (!path_var)
-        return (NULL);
-    if ((_strlen(command) > 2) && starts_with(command, "./"))
-    {
-        if (is_executable(info, command))
-        {
-            return (command);
-        }
-    }
-    while (1)
-    {
-        if (!path_var[i] || path_var[i] == ':')
-        {
-            full_path = extract_substring(path_var, current_pos, i);
-            if (!*full_path)
-            {
-                _strcat(full_path, command);
-            }
-            else
-            {
-                _strcat(full_path, "/");
-                _strcat(full_path, command);
-            }
-            if (is_executable(info, full_path))
-            {
-                return (full_path);
-            }
-            if (!path_var[i])
-            {
-                break;
-            }
-            current_pos = i;
-        }
-        i++;
-    }
-    return (NULL);
+if (!path_var)
+return (NULL);
+if ((_strlen(command) > 2) && starts_with(command, "./"))
+{
+if (is_executable(info, command))
+{
+return (command);
+}
+}
+while (1)
+{
+if (!path_var[i] || path_var[i] == ':')
+{
+full_path = extract_substring(path_var, current_pos, i);
+if (!*full_path)
+{
+_strcat(full_path, command);
+}
+else
+{
+_strcat(full_path, "/");
+_strcat(full_path, command);
+}
+if (is_executable(info, full_path))
+{
+return (full_path);
+}
+if (!path_var[i])
+{
+break;
+}
+current_pos = i;
+}
+i++;
+}
+return (NULL);
 }
--- shell.h
+++ shell.h
@@ -1,7 +1,29 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+typedef struct list_s {
+    char *str;
+    struct list_s *next;
+} list_t;
+
+typedef struct info {
+    char **argv;
+    int argc;
+    list_t *env;
+} info_t;
+
+
 int _myexit(info_t *info);
-int _mycd(info_t *info)
-int _myhelp(info_t *info);
+int _mycustom(info_t *info);
+int _mycd(info_t *info);
+int is_executable(info_t *info, char *file_path);
+char *extract_substring(char *source_str, 
+int start_idx, int end_idx);
+char *find_executable_in_path(info_t *info,
+char *path_var, char *command);
+int myenv(info_t *info);
+char *getEnvironmentVariable(info_t *info, const char *name);
+int mysetenv(info_t *info);
+int myunsetenv(info_t *info);
+int populateEnvironmentList(info_t *info);
 #endif
